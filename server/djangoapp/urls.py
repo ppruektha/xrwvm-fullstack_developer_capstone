6f6f8f706567064faa6b1e7928b5da79b0a1fccf
ppruektha <phaisit@126.com>
2024-10-04 11:28:05 -0400

    Build CarModel and CarMake Django Models

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -17,6 +17,8 @@ urlpatterns = [
 
     # path for dealer reviews view
 
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
+
     # path for add a review view
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
